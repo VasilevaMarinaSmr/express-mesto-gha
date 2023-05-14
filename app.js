ef0a6f283f487ce41f7fd257ea68caf242957990
Marina Vasileva <mv031a@yandex.ru>
2023-05-15 00:25:28 +0400

    feat: Создать контроллер card

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 app.use('/', require('./routes/users'));
+app.use('/', require('./routes/cards'));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
--- controllers/cards.js
+++ controllers/cards.js
@@ -1,19 +1,32 @@
-const Card = require('../models/card');
+const Card = require("../models/card");
 
-
-module.exports.getUsers = (req, res) => {
+module.exports.getCards = (req, res) => {
   Card.find({})
-  .populate(['owner', 'likes'])
-    .then(card => res.send({ data: card }))
-    .catch(() => res.status(500).send({ message: `Произошла ошибка ${err.message}` }));
+    .then((card) => res.send({ data: card }))
+    .catch((err) =>
+      res.status(500).send({ message: `Произошла ошибка ${err.message}` })
+    );
 };
 
+module.exports.createCard = (req, res) => {
+  const { name, link } = req.body;
+  Card.create({ name, link, owner: req.user._id })
+    .then((card) => res.send({ data: card }))
+    .catch((err) =>
+      res.status(500).send({ message: `Произошла ошибка ${err.message}` })
+    );
+};
 
-
-module.exports.createCard =(req, res) => {
-  const { name, link, ownerId  } = req.body;
-
-  Card.create({ name, link, owner: ownerId   })
-    .then(film => res.send({ data: film }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+module.exports.delCard = (req, res) => {
+  Card.findByIdAndRemove(req.params.cardId)
+    .then((card) => {
+      if (!card) {
+        res.send({ message: "Запрашиваемая карточка не найдена" });
+        return;
+      }
+      res.send(card);
+    })
+    .catch((err) =>
+      res.status(500).send({ message: `Произошла ошибка ${err.message}` })
+    );
 };
