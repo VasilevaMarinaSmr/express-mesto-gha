76d186d1132476c2b81a005fe23459af18bd6595
Marina Vasileva <mv031a@yandex.ru>
2023-05-16 17:43:22 +0400

    feat: Добавить обработку ошибок

--- controllers/cards.js
+++ controllers/cards.js
@@ -12,10 +12,16 @@ module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
   const owner = req.user._id;
   Card.create({ name, link, owner })
-    .then((card) => res.send({ data: card }))
-    .catch((err) =>
-      res.status(500).send({ message: `Произошла ошибка ${err.message}` })
-    );
+  .then((card) => res.status(200).send(card))
+  .catch((err) => {
+    if (err.name === 'ValidationError') {
+      res.status(400).send({
+        message: 'Переданы некорректные данные при создании карточки',
+      });
+    } else {
+      res.status(500).send({ message: `Произошла ошибка${err.message}` });
+    }
+  });
 };
 
 module.exports.delCard = (req, res) => {
--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,7 @@ const User = require("../models/user");
 
 module.exports.getUsers = (req, res) => {
   User.find({})
-    .then((user) => res.send( user ))
+    .then((user) => res.status(200).send( user ))
     .catch(() =>
       res.status(500).send({ message: `Произошла ошибка ${err.message}` })
     );
@@ -10,11 +10,25 @@ module.exports.getUsers = (req, res) => {
 
 module.exports.getUser = (req, res) => {
   User.findById(req.params.userId)
-    .then((user) => res.send( user ))
-    .catch((err) =>
-      res.status(500).send({ message: `Произошла ошибка ${err.message}` })
-    );
-};
+  .then((user) => {
+    if (!user) {
+      res.status(404).send({ message: 'Нет пользователя с таким id' });
+    } else {
+      res.status(200).send(user);
+    }
+  })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(400).send({
+          message: 'Переданы некорректные данные',
+        });
+    } else {
+      res.status(500).send({ message: `Произошла ошибка ${err.message}` });
+    }
+    });
+  };
+
+
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
