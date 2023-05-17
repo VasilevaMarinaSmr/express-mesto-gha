b991e58fe29b93bdae75e43fddb33e1d4d672366
Marina Vasileva <mv031a@yandex.ru>
2023-05-17 18:04:54 +0400

    feat: Добваить возможность изменения информации о пользователе

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use((req, res, next) => {
 });
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', require('./routes/users'));
 app.use('/', require('./routes/cards'));
 
--- controllers/users.js
+++ controllers/users.js
@@ -4,7 +4,7 @@ module.exports.getUsers = (req, res) => {
   User.find({})
     .then((user) => res.status(200).send( user ))
     .catch(() =>
-      res.status(500).send({ message: `Произошла ошибка ${err.message}` })
+      res.status(500).send({ message: `Ошибка сервера ${err.message}` })
     );
 };
 
@@ -12,7 +12,7 @@ module.exports.getUser = (req, res) => {
   User.findById(req.params.userId)
   .then((user) => {
     if (!user) {
-      res.status(404).send({ message: 'Нет пользователя с таким id' });
+      res.status(404).send({ message: 'Пользователь с указаным id не найден' });
     } else {
       res.status(200).send(user);
     }
@@ -23,7 +23,7 @@ module.exports.getUser = (req, res) => {
           message: 'Переданы некорректные данные',
         });
     } else {
-      res.status(500).send({ message: `Произошла ошибка ${err.message}` });
+      res.status(500).send({ message: `Ошибка сервера ${err.message}` });
     }
     });
   };
@@ -35,6 +35,61 @@ module.exports.createUser = (req, res) => {
   User.create({ name, about, avatar })
     .then((user) => res.send( user ))
     .catch((err) =>
-      res.status(500).send({ message: `Произошла ошибка ${err.message}` })
+      res.status(500).send({ message: `Ошибка сервера ${err.message}` })
     );
 };
+
+module.exports.updateProfile = (req, res) => {
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
+    .then((user) => {
+      if (!user) {
+        res
+          .status(404)
+          .send({ message: 'Пользователь с указаным id не найден' });
+      } else {
+        res.status(200).send(user);
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(400).send({
+          message: 'Переданы некорректные данные при обновлении профиля.',
+        });
+      } else {
+        res.status(500).send({ message: `Ошибка сервера ${err.message}` });
+      }
+    });
+};
+
+module.exports.updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+  User.findByIdAndUpdate(
+    req.user._id,
+    { avatar },
+    {
+      new: true,
+      runValidators: true,
+    }
+  )
+    .then((user) => {
+      if (!user) {
+        res
+          .status(404)
+          .send({ message: 'Пользователь с указанным _id не найден.' });
+      } else {
+        res.status(200).send(user);
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(400).send({
+          message: 'Переданы некорректные данные',
+        });
+      } else {
+        res.status(500).send({ message: `Произошла ошибка ${err.message}` });
+      }
+    });
+};
+
+
