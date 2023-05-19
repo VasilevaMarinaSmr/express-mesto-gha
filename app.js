ad98bd4ff31cb3ddf2ff540d230cf1be75dc2cf7
Marina Vasileva <mv031a@yandex.ru>
2023-05-19 16:51:05 +0400

    FIX: Добавить константы ошибок

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const { PORT = 3000 } = process.env;
-const ERROR_DATA_NOT_FOUND = require('./utils/errors');
+const { ERROR_DATA_NOT_FOUND } = require('./utils/errors');
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
