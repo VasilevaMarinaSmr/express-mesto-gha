95c7baa52815fb83437206e02180d45a93df9ad8
Marina Vasileva <mv031a@yandex.ru>
2023-06-01 09:05:00 +0400

    FIX: Ошибки

--- controllers/users.js
+++ controllers/users.js
@@ -5,50 +5,54 @@ const errorRequest  = require("../errors/error-request");
 const errorDataNotFound = require("../errors/datat-not-found-err");
 
 
-const {
-  ERROR_REQUEST,
-  ERROR_DATA_NOT_FOUND,
-  SERVER_ERROR,
-} = require("../utils/errors");
+//const {
+//  ERROR_REQUEST,
+//  ERROR_DATA_NOT_FOUND,
+//  SERVER_ERROR,
+// } = require("../utils/errors");
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 
-module.exports.getUsers = (req, res) => {
+module.exports.getUsers = (req, res, next) => {
   User.find({})
     .then((user) => res.send(user))
-    .catch(() =>
-      res.status(SERVER_ERROR).send({ message: "Ошибка по умолчанию." })
-    );
+    .catch(next);
 };
 
 module.exports.getUser = (req, res) => {
   User.findById(req.params.userId)
     .then((user) => {
       if (!user) {
-        res
-          .status(ERROR_DATA_NOT_FOUND)
-          .send({ message: "Пользователь по указанному _id не найден." });
-      } else {
+        if (!user) {
+          throw new NotFoundError('Пользователь по указанному _id не найден.');
+        }
         res.send(user);
-      }
-    })
-    .catch((err) => {
-      if (err.name === "CastError") {
-        res
-          .status(ERROR_REQUEST)
-          .send({ message: "Переданы некорректные данные." });
-      } else {
-        res.status(SERVER_ERROR).send({ message: "Ошибка по умолчанию" });
-      }
-    });
-};
+      }})
+      .catch(next);
+  };
+
+
+
+
+
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar, email, password } = req.body;
+
+
   bcrypt
     .hash(password, 10)
     .then((hash) => User.create({ name, about, avatar, email, password: hash }))
+    .catch((err) => {
+      if (err.name === 'MongoError' || err.code === 11000) {
+        throw new ConflictError(
+          'Пользователь с таким email уже зарегистрирован'
+        );
+      } else next(err);
+
+
+
     .then((user) => res.send(user))
     .catch((err) => {
       if (err.name === "ValidationError") {
