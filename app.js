b169de84fecc7a00a5fbb5be0348c32bc32d3bc0
Marina Vasileva <mv031a@yandex.ru>
2023-06-03 03:35:13 +0400

    FIX: Исправить ошибки 1

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.post(
   "/signin",
   celebrate({
     body: Joi.object().keys({
-      email: Joi.string().required(),
+      email: Joi.string().required().email(),
       password: Joi.string().required(),
     }),
   }),
@@ -36,8 +36,8 @@ app.post(
     body: Joi.object().keys({
       name: Joi.string().min(2).max(30),
       about: Joi.string().min(2).max(30),
-      avatar: Joi.string().pattern(urlTemplate),
-      email: Joi.string().required(),
+      avatar: Joi.string().regex(urlTemplate),
+      email: Joi.string().required().email(),
       password: Joi.string().required(),
     }),
   }),
